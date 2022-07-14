3028b7eb239627e74389c578fb91cc9d463ee0cc
Frapples <107868453+0xFrapples@users.noreply.github.com>
2022-07-14 23:23:58 +0300

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,8 @@
+a=int(input("Enter the first number: "))
+b=int(input("Enter the second number: "))
+
+def larger_number():
+    if a > b:
+        print ("true")
+    else:
+        print ("false")
